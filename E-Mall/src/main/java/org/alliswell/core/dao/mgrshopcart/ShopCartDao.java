6b16038f44bad68db67191caba850d75b94fd53a
alliswell008 <1240129405@qq.com>
2016-11-21 23:20:23 +0800

    添加E-Mall结构代码

---
+++ E-Mall/src/main/java/org/alliswell/core/dao/mgrshopcart/ShopCartDao.java
@@ -0,0 +1,18 @@
+package org.alliswell.core.dao.mgrshopcart;
+
+/**
+ * @ Title: ShopCartDao
+ * @ Package org.alliswell.core.dao.mgrshopcart
+ * @ Description: TODO(用一句话描述该文件做什么)
+ * @ author alliswell
+ * @ date 2016/11/21 23:08
+ * @ version V1.0
+ * @ Update_Logs:
+ * ****************************************************
+ * Name:
+ * Date:
+ * Description:
+ * *****************************************************
+ */
+public class ShopCartDao {
+}
---
+++ E-Mall/src/main/java/org/alliswell/core/dao/mgrware/WareDao.java
@@ -0,0 +1,18 @@
+package org.alliswell.core.dao.mgrware;
+
+/**
+ * @ Title: WareDao
+ * @ Package org.alliswell.core.dao.mgrware
+ * @ Description: TODO(用一句话描述该文件做什么)
+ * @ author alliswell
+ * @ date 2016/11/21 23:09
+ * @ version V1.0
+ * @ Update_Logs:
+ * ****************************************************
+ * Name:
+ * Date:
+ * Description:
+ * *****************************************************
+ */
+public class WareDao {
+}
