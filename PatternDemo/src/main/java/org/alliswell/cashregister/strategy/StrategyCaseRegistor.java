33d8db7775b9929e619e21fdf2850f82f3db1dbe
cxs <1240129405@qq.com>
2016-11-24 22:23:41 +0800

    PatternDemo：商场收银策略模式

---
+++ PatternDemo/src/main/java/org/alliswell/cashregister/strategy/StrategyCaseRegistor.java
@@ -0,0 +1,24 @@
+/**
+ * @Title: StrategyCaseRegistor
+ * @Package org.alliswell.cashregister.strategy
+ * @Description: TODO(用一句话来说明)
+ * @author alliswell008
+ * @date 2016/11/24 22:17
+ * @version V1.0
+ * Update Logs:
+ * ****************************************************
+ * Name:
+ * Date:
+ * Description:
+ * *****************************************************
+ */
+package org.alliswell.cashregister.strategy;
+
+/**
+ * @author alliswell008
+ * @ClassName: StrategyCaseRegistor
+ * @Description: TODO(用一句话来说明)
+ * @date 2016/11/24 22:17
+ */
+public class StrategyCaseRegistor {
+}
