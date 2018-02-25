8cf77c19b0e7f566e33127fa35120727995e8dcf
cxs <1240129405@qq.com>
2018-02-25 17:33:36 +0800

    volatile

--- JavaDemo/src/main/java/test/TestA.java
+++ JavaDemo/src/main/java/test/TestA.java
@@ -14,28 +14,38 @@ class StaticTest
     // {
     //     staticFunction();
     // }
- 
+
     static StaticTest st = new StaticTest();
- 
+
     static
     {
         System.out.println("1");
     }
- 
+
     {
         System.out.println("2");
     }
- 
-    StaticTest()
+
+    public StaticTest()
     {
         System.out.println("3");
         System.out.println("a="+a+",b="+b);
     }
- 
+
     public static void staticFunction(){
         System.out.println("4");
+        System.out.println("b="+b);
     }
- 
+
     int a=110;
     static int b =112;
+
+
 }
+
+// 2
+// 3
+// a=110,b=0
+// 1
+// 4
+// b=112
