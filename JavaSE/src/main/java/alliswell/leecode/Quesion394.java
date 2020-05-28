783a021e7281f510d6f4acf4d3d08785e3aa4d08
allisx <alliswill@foxmail.com>
2020-05-28 23:57:10 +0800

    junit

--- JavaSE/src/main/java/alliswell/leecode/Quesion394.java
+++ JavaSE/src/main/java/alliswell/leecode/Quesion394.java
@@ -22,7 +22,7 @@ import java.util.Stack;
  * 链接：https://leetcode-cn.com/problems/decode-string
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class Class394 {
+public class Quesion394 {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
